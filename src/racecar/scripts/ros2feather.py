6367bf60544e0d3c51a62e4d8ef87575ffb38e43
racecar <wangweizhuo2012@gmail.com>
2023-08-24 18:34:12 -0700

    more telemetry and more robust to Ctrl C

--- src/racecar/scripts/ros2feather.py
+++ src/racecar/scripts/ros2feather.py
@@ -3,6 +3,19 @@ import serial
 import time
 import rospy
 from std_msgs.msg import Float32
+import signal
+
+# ===========Killer=========
+def signal_handler(signal, frame):
+    global loop_running
+    loop_running = False
+    # Close the serial port.
+    ser.close()
+    exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+loop_running = True
+# ==========================
 
 def steer_callback(data):
     global steer_angle
@@ -11,6 +24,7 @@ def steer_callback(data):
 HZ = 50
 rospy.init_node('FeatherSerial', anonymous=True)
 rospy.Subscriber("steer", Float32, steer_callback)
+rate = rospy.Rate(HZ)
 steer_angle = 85.0
 
 # Open the serial port.
@@ -26,29 +40,30 @@ t2 = time.time()
 print('Sent command:')
 
 # Read the response.
-while True:
-    t1 = time.time()
-    print('\n[============]')
-    response = ser.readline().decode()
-    print("RS:",response)
-    while ser.in_waiting:
+while loop_running:
+    try:
+        t1 = time.time()
+        print('\n[============]')
         response = ser.readline().decode()
-        if response[0] == "R":
-            print('Response:', response)
-            # print(time.time()-t2)
-            # print('dd',time.time()-t1)
-    
-    cmd = (str(int(steer_angle))+"\n").encode()
-    print(cmd)
-    ser.write(cmd)
-    ser.flush()
-    print('Done Writing')
+        print("RS:",response)
+        while ser.in_waiting:
+            response = ser.readline().decode()
+            if response[0] == "R":
+                print('Response:', response)
+                # print(time.time()-t2)
+                # print('dd',time.time()-t1)
+        
+        cmd = (str(int(steer_angle))+"\n").encode()
+        print(cmd)
+        ser.write(cmd)
+        ser.flush()
+        print('Done Writing')
 
-    # t2 = time.time()
-    # time.sleep(max(0,1/HZ-(t2-t1)-0.00006))
-    # t3 = time.time()
-    # print('Time:',t3-t1)
-    time.sleep(0.02)
+        # t2 = time.time()
+        # time.sleep(max(0,1/HZ-(t2-t1)-0.00006))
+        # t3 = time.time()
+        # print('Time:',t3-t1)
+        rate.sleep()
 
-# Close the serial port.
-ser.close()
+    except Exception as e:
+        print(f"Killing for: {e}")
--- src/racecar/scripts/ros2vesc.py
+++ src/racecar/scripts/ros2vesc.py
@@ -3,6 +3,39 @@ import serial,time
 from pyvesc import VESC
 import rospy
 from std_msgs.msg import Float32
+import numpy as np
+import signal
+
+# ===========Killer=========
+def signal_handler(signal, frame):
+    global loop_running
+    loop_running = False
+    exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+loop_running = True
+# ==========================
+
+def lipo_volt2percent(voltage):
+    if voltage < 9.82:
+        return -1
+    # Typical voltage levels for a single-cell LiPo
+    voltage_points = [9.82, 10.83, 11.06, 11.12, 11.18,
+                      11.24, 11.3, 11.36, 11.39, 11.45,
+                      11.51, 11.56, 11.62, 11.74, 11.86, 
+                      11.95, 12.07, 12.25, 12.33, 12.45, 12.6]
+    # percentage_points = [0, 10, 20, 40, 60, 80, 100]
+    percentage_points = list(range(0,105,5))
+
+    # Find the two points in the curve that the voltage lies between
+    for i in range(1, len(voltage_points)):
+        if voltage < voltage_points[i]:
+            # Linearly interpolate between the two points
+            percentage = percentage_points[i-1] + (percentage_points[i] - percentage_points[i-1]) * (voltage - voltage_points[i-1]) / (voltage_points[i] - voltage_points[i-1])
+            return percentage
+
+    # If voltage is greater than the maximum voltage point, return 100
+    return 100.0
 
 throttle = 0.0
 
@@ -16,14 +49,53 @@ vesc = VESC(serial_port)
 
 rospy.init_node('VESCSerial', anonymous=True)
 rospy.Subscriber("throttle", Float32, throttle_callback)
+rate = rospy.Rate(50) # 10 Hz
+
+v_in_pub  = rospy.Publisher('V_in', Float32, queue_size=1)
+rpm_pub   = rospy.Publisher('RPM', Float32, queue_size=1)
+duty_pub  = rospy.Publisher('Duty_cycle', Float32, queue_size=1)
+tach_pub  = rospy.Publisher('Tach', Float32, queue_size=1)
+power_pub = rospy.Publisher('P_motor', Float32, queue_size=1)
+batt_pub  = rospy.Publisher('Batt', Float32, queue_size=1)
+# averages
+vin_lst = []
 
 count = 0
-while True:
-    print(throttle)
-    vesc.set_rpm(throttle*30)
-    time.sleep(0.02)
-    count += 1
+while loop_running:
+    try:
+        if not (throttle is None):
+            print("Throttle ",throttle)
+            if throttle < 0.05:
+                vesc.set_current(0.0)
+            else:
+                vesc.set_rpm(int(throttle*10000)) # minimum is 588erpm, 9.8rps
 
-    if count%50 == 0:
-        status = vesc.get_measurements()
-        print(status)
+        if count%1 == 0:
+            status = vesc.get_measurements()
+            if status is not None:
+                # update values
+                vin_lst.append(status.v_in)
+
+                batt_percent = lipo_volt2percent(np.mean(vin_lst))
+
+                # publish values
+                v_in_pub.publish(np.mean(vin_lst))
+                batt_pub.publish(batt_percent)
+                rpm_pub.publish(status.rpm)
+                duty_pub.publish(status.duty_cycle_now)
+                tach_pub.publish(status.tachometer)
+                power_pub.publish(status.avg_input_current*status.v_in)
+
+                # manage lists
+                if len(vin_lst) > 400:
+                    vin_lst.pop(0)
+                
+                # print("Duty:",status.duty_cycle_now)
+            else:
+                print('Status is none')
+
+        count += 1
+        rate.sleep()
+
+    except Exception as e:
+        print(f"Killing for: {e}")
